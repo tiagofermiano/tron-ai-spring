ed7e4bb040f084dcbeb5fc8e136c14b636c64330
tiagofermiano <tiagomachadofermiano@gmail.com>
2025-11-28 00:03:27 -0300

    projeto tron IA

---
+++ src/main/java/com/clout/tron/TronApplication.java
@@ -0,0 +1,13 @@
+package com.clout.tron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TronApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TronApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clout/tron/TronApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clout.tron;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TronApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
